1f8ff325b46f4b052d71eb6b036158d9f0c3117f
suhailmajeed-pixel <suhailmajeed@gmail.com>
2026-04-03 18:41:23 +0530

    Add files via upload

---
+++ Class_Vehicle.py
@@ -0,0 +1,8 @@
+class vehical:
+    def __init__(self , max_speed , mileag):
+        self.max_speed = max_speed
+        self.mileag = mileag 
+
+modelX = vehical(240,18)
+print ("model max speed ",modelX.max_speed)
+print ("model mileag ",modelX.mileag)
